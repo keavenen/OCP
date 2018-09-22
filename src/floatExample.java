e03f219c5a35568046c74989e507f603e96b3f04
keavenen <keaveneyniall@gmail.com>
2018-09-22 23:49:05 +0100

    Sample file

---
+++ src/floatExample.java
@@ -0,0 +1,9 @@
+public class floatExample {
+    public static void main(String[] args) {
+        float value1 = 102;
+        float value2 = (int)102.0;
+        //float value3 = 1f * 0.0;
+        float value4 = 1f * (short)0.0;
+        //float value5 = 1f * (boolean)0;
+    }
+}
